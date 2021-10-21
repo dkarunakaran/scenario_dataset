387b50ab78cfd99a19575ed124279a7b35dcb6b5
Dhanoop Karunakaran <dkar9051@uni.sydney.edu.au>
2021-10-21 07:19:44 +0000

    Overtaking scenario implementation started

--- catkin_ws/src/extraction/src/extraction.cpp
+++ catkin_ws/src/extraction/src/extraction.cpp
@@ -11,9 +11,19 @@
 #include <rosbag/view.h>
 #include <geometry_msgs/Pose.h>
 #include <nav_msgs/Odometry.h>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 #include <tf/tf.h>
 #include <ibeo_object_msg/IbeoObject.h> 
+#include <lanelet2_core/LaneletMap.h>
+#include <lanelet2_core/geometry/Lanelet.h>
+#include <lanelet2_core/primitives/Lanelet.h>
+#include <lanelet2_core/primitives/LineString.h>
+#include <lanelet2_core/primitives/Point.h>
+#include <lanelet2_io/Io.h>
+#include <lanelet2_io/io_handlers/Factory.h>
+#include <lanelet2_io/io_handlers/Writer.h>
+#include <lanelet2_projection/UTM.h>
+#include <tf2_ros/transform_listener.h>
 #include "helper_functions.hpp"
 
 using json = nlohmann::json;
@@ -27,13 +37,15 @@ class Extraction : public dataset_toolkit::h264_bag_playback {
     std::chrono::steady_clock::time_point start_time, end_time;
     std::string odometry_topic; std::string objects_topic;
     std::string bag_file;
-    std::vector<uint32_t> odomTimeStamp; //std::vector<geometry_msgs::Pose> odomPos; 
+    std::vector<uint32_t> odomTimeStamp;
     std::vector<json> odomPos;
-    std::vector<uint32_t> objectsTimeStamp; //std::vector<geometry_msgs::Pose> objectsPos; 
+    std::vector<uint32_t> objectsTimeStamp;
     std::vector<json> objectsPos;
     bool resume;
     bool storeDataInitially;
     std::string odom_json_file;std::string objects_json_file;
+    std::string lanelet_file; lanelet::LaneletMapPtr map;
+    std::vector<uint32_t> egoSec;
 
     Extraction() : h264_bag_playback() {
       previous_percentage = -1;
@@ -44,68 +56,78 @@ class Extraction : public dataset_toolkit::h264_bag_playback {
       private_nh.getParam("resume", resume);
       private_nh.getParam("odom_json_file", odom_json_file);
       private_nh.getParam("objects_json_file", objects_json_file);
+      private_nh.getParam("lanelet_file", lanelet_file);
+      lanelet::projection::UtmProjector projector(lanelet::Origin({0, 0}));  // we will go into details later
+      map = lanelet::load(lanelet_file, projector);
+      //Below code is an example and do not delete
+      /*try {
+        geometry_msgs::PointStamped baseLinkPoint;
+        baseLinkPoint.point.x = egoPosX;baseLinkPoint.point.y = egoPosY;baseLinkPoint.point.z = 0;
+        baseLinkPoint.header.frame_id = "base_link";baseLinkPoint.header.stamp.sec = sec;
+        geometry_msgs::PointStamped odomPoint;
+        transformer_->setUsingDedicatedThread(true); 
+        transformer_->transform(baseLinkPoint, odomPoint, "odom");
+      }catch (const std::exception &e) {
+        ROS_ERROR_STREAM(e.what());
+      }*/
     }
 
-    void storeData(){
-      if(!resume){
-        //Converting the vector to json
-        json odomJson(odomPos);json objJson(objectsPos);
-        // write prettified JSON to a file
-        std::ofstream o1(odom_json_file);
-        o1 << std::setw(4) << odomJson << std::endl;
-        std::ofstream o2(objects_json_file);
-        o2 << std::setw(4) << objJson << std::endl;
+    void loadData(){
+      // read a JSON file
+      std::ifstream i1(odom_json_file);
+      json j1;i1 >> j1; 
+      for (auto& item : j1) {
+        odomPos.push_back(item);
+        auto sec = item["sec"].get<int>();
+        odomTimeStamp.push_back(sec);
       }
+      std::ifstream i2(objects_json_file);
+      json j2;i2 >> j2;
+      for (auto& item : j2) {
+        objectsPos.push_back(item);
+        auto sec = item["sec"].get<int>();
+        objectsTimeStamp.push_back(sec);
+      }
+      
+      ROS_INFO_STREAM("Odometry size: "<<odomPos.size()<<" Objects timestamp: "<<objectsPos.size());
+    }
+    
+    void storeData(){
+      //Converting the vector to json
+      json odomJson(odomPos);json objJson(objectsPos);
+      // write prettified JSON to a file
+      std::ofstream o1(odom_json_file);
+      o1 << std::setw(4) << odomJson << std::endl;
+      std::ofstream o2(objects_json_file);
+      o2 << std::setw(4) << objJson << std::endl;
       ROS_INFO_STREAM("Odometry size: "<<odomPos.size()<<" Objects timestamp: "<<objectsPos.size());
     }
          
     void MessagePublisher(ros::Publisher &publisher, const rosbag::MessageInstance &message) {
       if(storeDataInitially){
-        if(resume){
-          // read a JSON file
-          std::ifstream i1(odom_json_file);
-          json j1;i1 >> j1; 
-          for (auto& item : j1) {
-            odomPos.push_back(item);
-            auto sec = item["sec"].get<int>();
-            odomTimeStamp.push_back(sec);
-          }
-          
-          std::ifstream i2(objects_json_file);
-          json j2;i2 >> j2;
-          for (auto& item : j2) {
-            objectsPos.push_back(item);
-            auto sec = item["sec"].get<int>();
-            objectsTimeStamp.push_back(sec);
-          }
-        
-        }else{
-          if(message.getTopic() == odometry_topic){
-            nav_msgs::Odometry::ConstPtr odomPtr = message.instantiate<nav_msgs::Odometry>();
-            if(odomPtr != nullptr){
-              if(std::find(odomTimeStamp.begin(), odomTimeStamp.end(), odomPtr->header.stamp.sec) != odomTimeStamp.end()){
-              }else{
-                json jData = constructJsonData(odomPtr);
-                odomPos.push_back(jData);  
-                odomTimeStamp.push_back(odomPtr->header.stamp.sec);
-                ROS_INFO_STREAM("Storing... Time in sec: "<<odomPtr->header.stamp.sec);
-              }
+        if(message.getTopic() == odometry_topic){
+          nav_msgs::Odometry::ConstPtr odomPtr = message.instantiate<nav_msgs::Odometry>();
+          if(odomPtr != nullptr){
+            if(std::find(odomTimeStamp.begin(), odomTimeStamp.end(), odomPtr->header.stamp.sec) != odomTimeStamp.end()){
+            }else{
+              json jData = constructJsonData(odomPtr, transformer_);
+              odomPos.push_back(jData);  
+              odomTimeStamp.push_back(odomPtr->header.stamp.sec);
+              ROS_INFO_STREAM("Storing... Time in sec: "<<odomPtr->header.stamp.sec);
             }
-          }//odometry topic if close
-          if(message.getTopic() == objects_topic){
-            ibeo_object_msg::IbeoObject::ConstPtr objPtr = message.instantiate<ibeo_object_msg::IbeoObject>();
-            if(objPtr != nullptr){
-              if(std::find(objectsTimeStamp.begin(), objectsTimeStamp.end(), objPtr->header.stamp.sec) != objectsTimeStamp.end()){
-              }else{
-                json jData = constructJsonData(objPtr);
-                objectsPos.push_back(jData);  
-                objectsTimeStamp.push_back(objPtr->header.stamp.sec);  
-              }
+          }
+        }//odometry topic if close
+        if(message.getTopic() == objects_topic){
+          ibeo_object_msg::IbeoObject::ConstPtr objPtr = message.instantiate<ibeo_object_msg::IbeoObject>();
+          if(objPtr != nullptr){
+            if(std::find(objectsTimeStamp.begin(), objectsTimeStamp.end(), objPtr->header.stamp.sec) != objectsTimeStamp.end()){
+            }else{
+              json jData = constructJsonData(objPtr, transformer_);
+              objectsPos.push_back(jData);  
+              objectsTimeStamp.push_back(objPtr->header.stamp.sec);  
             }
-          }//object topic if close 
-          
-         
-        }//resume else close
+          }
+        }//object topic if close 
       }else{
         current_message_number++;
         int current_percentage = 100.0 * ((float) current_message_number / (float) total_message_count);
@@ -122,14 +144,55 @@ class Extraction : public dataset_toolkit::h264_bag_playback {
           start_time = std::chrono::steady_clock::now();
         }
         
-        /*if(message.getTopic() == objects_topic){
-          ROS_INFO_STREAM("Topic: "<<message.getTopic());
-        }else if(message.getTopic() == odometry_topic){
-          ROS_INFO_STREAM("Topic: "<<message.getTopic());
-        }*/
-        // Do the lane change detection
+        laneChangeDetection(message);
       }
     }//MessagePublisher method closes
+
+    void laneChangeDetection(const rosbag::MessageInstance &msg){
+      if(msg.getTopic() == odometry_topic){
+        nav_msgs::Odometry::ConstPtr odomPtr = msg.instantiate<nav_msgs::Odometry>();
+        if(std::find(egoSec.begin(), egoSec.end(), odomPtr->header.stamp.sec) != egoSec.end()){
+        }else{
+          auto sec = odomPtr->header.stamp.sec; 
+          std::vector<uint32_t> projectedSec;
+          for(size_t i=sec+1; i<=sec+8; i++){
+            if(checkSecExist(odomTimeStamp, i))
+              projectedSec.push_back(i);
+          }
+          
+          //1. Overtaking scenario
+          overtakingScenario(sec, projectedSec);   
+          
+          egoSec.push_back(sec);
+        }
+      }//Odometry topic if closes
+    }
+
+    void overtakingScenario(uint32_t sec, std::vector<uint32_t> projectedSec){
+      
+      //1. Find the number lines where the car is at the current sec
+      
+      for(auto& sec: projectedSec){
+        auto index = findIndex(odomTimeStamp, sec);  
+        auto egoJsonData = odomPos[index];
+        double egoPosX = egoJsonData["position_x"];double egoPosY = egoJsonData["position_y"]; 
+        auto egoPoint = lanelet::BasicPoint2d(egoPosX, egoPosY);
+        //1. identify the number of lanes in the projected sec
+        //a. identify the lanelet that the search point is in
+        auto nearLanelets = findTheActualLanelet(map, egoPoint);
+        if(std::get<0>(nearLanelets) && lanelet::geometry::length3d(std::get<1>(nearLanelets)) > 0){
+          auto egoLanelet = std::get<1>(nearLanelets);
+          auto lanelets = std::get<2>(nearLanelets);
+          //b. find the lanelet that is left and right 
+        
+        
+        }//Checking lanelet for the egopoint if closes      
+        else{
+          ROS_INFO_STREAM("No lanelet found: length of the lanelet is zero or simply couldn't find the lanelet that actually has the point");
+        } 
+
+      }
+    }
 };
 
 
@@ -139,9 +202,16 @@ int main(int argc, char **argv) {
 
   Extraction extract;
   extract.init_playback();
-  extract.storeDataInitially = true;  
-  extract.ReadFromBag();
-  extract.storeData();
+  ROS_INFO_STREAM("Resume: "<<extract.resume);
+  if(extract.resume){
+    ROS_INFO_STREAM("Data is loading from JSON files...");
+    extract.loadData();
+  }else{
+    ROS_INFO_STREAM("Data is loading from Bag file...");
+    extract.storeDataInitially = true;  
+    extract.ReadFromBag();
+    extract.storeData();
+  }
   extract.storeDataInitially = false;
   extract.ReadFromBag();
   
--- catkin_ws/src/extraction/src/helper_functions.hpp
+++ catkin_ws/src/extraction/src/helper_functions.hpp
@@ -3,7 +3,7 @@
 
 using json = nlohmann::json;
 
-json constructJsonData(nav_msgs::Odometry::ConstPtr dataPtr){
+json constructJsonData(nav_msgs::Odometry::ConstPtr dataPtr, std::shared_ptr<tf2_ros::Buffer> transformer_){
   json jData;
   tf::Quaternion q(
     dataPtr->pose.pose.orientation.x,
@@ -13,6 +13,25 @@ json constructJsonData(nav_msgs::Odometry::ConstPtr dataPtr){
   tf::Matrix3x3 m(q);
   double roll, pitch, yaw;
   m.getRPY(roll, pitch, yaw);
+  //auto world_transform = transformer_->lookupTransform(std::string("base_link_horizon"), std::string("base_link"), dataPtr->header.stamp);
+ /* Eigen::Quaternionf world_rotation(world_transform.transform.rotation.w,
+                                      world_transform.transform.rotation.x,
+                                      world_transform.transform.rotation.y,
+                                      world_transform.transform.rotation.z);
+
+  Eigen::Vector3f world_origin(world_transform.transform.translation.x,
+                                 world_transform.transform.translation.y,
+                                 world_transform.transform.translation.z);
+  
+  pcl::PointCloud<pcl::PointXYZ> pointCloud;
+  pcl::PointXYZ point;
+  point.x = dataPtr->pose.pose.position.x;
+  point.y = dataPtr->pose.pose.position.y;
+  point.z = dataPtr->pose.pose.position.z;
+  pointCloud.push_back(point);
+  pcl::transformPointCloud(pointCloud, pointCloud, world_origin, world_rotation);
+*/
+
   jData["linear_x"] = dataPtr->twist.twist.linear.x;
   jData["linear_y"] = dataPtr->twist.twist.linear.y;
   jData["linear_z"] = dataPtr->twist.twist.linear.z;
@@ -27,7 +46,7 @@ json constructJsonData(nav_msgs::Odometry::ConstPtr dataPtr){
   return jData;
 }
 
-json constructJsonData(ibeo_object_msg::IbeoObject::ConstPtr dataPtr){
+json constructJsonData(ibeo_object_msg::IbeoObject::ConstPtr dataPtr, std::shared_ptr<tf2_ros::Buffer>& transformer_){
   json jData;
   tf::Quaternion q(
     dataPtr->pose.pose.orientation.x,
@@ -51,5 +70,50 @@ json constructJsonData(ibeo_object_msg::IbeoObject::ConstPtr dataPtr){
 
   return jData;
 }
+
+
+bool checkSecExist(std::vector<uint32_t> timeStamp, uint32_t sec){
+  bool _return = false;
+  if(std::find(timeStamp.begin(), timeStamp.end(), sec) != timeStamp.end())  {
+    _return = true;
+  }
+  
+  return _return;
+}
+
+int findIndex(std::vector<uint32_t> v, uint32_t sec){
+  int _return = -1;  
+  auto it = find(v.begin(), v.end(), sec);
+ 
+  // If element was found
+  if (it != v.end())
+    _return = it - v.begin();
+
+  return _return;
+}
+
+std::tuple<bool, lanelet::Lanelet, std::vector<lanelet::Lanelet>> findTheActualLanelet(lanelet::LaneletMapPtr map, lanelet::BasicPoint2d egoPoint){
+  std::vector<std::pair<double, lanelet::Lanelet>> nearLanelets = lanelet::geometry::findNearest(map->laneletLayer, egoPoint, 4);
+  bool llFound = false;
+  size_t selected = 0;
+  std::vector<lanelet::Lanelet> lanelets;
+  for(size_t i=0; i<nearLanelets.size(); i++){
+    auto ll = nearLanelets[i];
+    lanelets.push_back(ll.second);
+    if(lanelet::geometry::inside(ll.second, egoPoint)){
+      llFound = true;
+      selected = i;
+      break;
+    }
+  }
+  
+  if(llFound)
+    return std::make_tuple(true, nearLanelets[selected].second, lanelets);
+  else{
+    lanelet::Lanelet tempLL;
+    return std::make_tuple(true, tempLL, lanelets);
+  }
+}
+
 
 #endif //APPLICATIONS_HELPER_FUNCTIONS_HPP
